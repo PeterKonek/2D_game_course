efdb6f12ff89089243b02ce8f3d64d9e727dbe70
PeterKonek <b.peterkonek@gmail.com>
2022-11-04 21:56:34 +0100

    First commit

---
+++ Scripts/QuestionSO.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Quiz question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2, 8)]
+    [SerializeField] string question = "Enter new question name here.";
+    [SerializeField] string[] answers = new string[4]; 
+    [SerializeField]int correctAnswerIndex;
+     
+
+    public string GetQuestion()
+    {
+        return question;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
+   
+}
